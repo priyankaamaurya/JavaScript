e8a4f6e8d41c139fc1ecc5245d50833c562c8265
priyanka <prachii8826@gmail.com>
2026-04-12 19:40:16 +0530

    added files

---
+++ Assessment/Assessment 4/script.js
@@ -0,0 +1,21 @@
+//? Arrays
+
+let arr = ["lily", "tulip", "jasmine", "lotus"]
+
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr)
+
+arr.push("habiscus");
+console.log(arr)
+
+arr.pop()
+console.log(arr)
+
+arr.unshift("waterlily")
+console.log(arr)
+
+arr.shift()
+console.log(arr)
