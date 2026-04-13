43093e0984522c673111b5cbf07483d47170e731
priyanka <prachii8826@gmail.com>
2026-04-13 23:33:20 +0530

    added files

--- Assessment/Assessment 4/script.js
+++ Assessment/Assessment 4/script.js
@@ -27,3 +27,47 @@ console.log(arr)
 arr.forEach((a)=>{
     console.log(a)
 });
+
+//? Objects 
+
+let emp = {
+    empId : "EW23006",
+    empName : "Priyanka",
+    dept : "IT"
+}
+
+//! Access Object
+
+console.log(emp)
+console.log(emp.empId)
+console.log(emp["dept"])
+
+//! Update Object
+emp.empName = "Prachi"
+console.log(emp)
+
+//? for loop
+
+for(let i = 1; i <= 5; i++){
+    console.log(i)
+}
+
+//? for...of (Arrays)
+
+for (let flower of arr){
+    console.log(flower)
+}
+
+//? for...in (Objects)
+
+for(let e in emp){
+    console.log(e, emp[e])
+}
+
+//? for Each
+
+let num =[1,2,3,4,5,6]
+
+num.forEach((num)=> {
+    console.log(num * 2)
+})
