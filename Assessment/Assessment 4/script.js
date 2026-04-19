142778916ed49fd6941aa753ac0c02adc5ce3b61
priyanka <prachii8826@gmail.com>
2026-04-19 23:16:13 +0530

    added files

--- Assessment/Assessment 4/script.js
+++ Assessment/Assessment 4/script.js
@@ -75,3 +75,4 @@ num.forEach((num)=> {
 
 
 
+
---
+++ Assessment/Assessment 5/script.js
@@ -0,0 +1,24 @@
+//? Questions on array
+
+//! 1. Find Maximum Number in Array
+
+// let arr = [11,2,3,4,5,6,7,8,9]
+
+// function findMax(arr){
+//     return Math.max(...arr)
+// }
+
+// let res = findMax(arr)
+// console.log(res)
+
+//! 2. Sum of All Elements
+
+// let arr = [11,2,3,5,6,7,9]
+
+// function sumArr(arr) {
+//     return arr.reduce((sum, num) => sum + num, 0)
+// }
+
+// let res = sumArr(arr)
+// console.log(res)
+
