3b92b6e276c7e404c5e328975b911835c84c6e75
priyanka <prachii8826@gmail.com>
2026-04-22 21:36:14 +0530

    added files

---
+++ Assessment/Assessment 6/script.js
@@ -0,0 +1,38 @@
+//! 1. Two Sum
+
+// let arr = [2,7,11,15];
+// let target = 9;
+
+// function twoSum(arr, target) {
+//     let map = {};
+
+//     for(let i=0; i<arr.length; i++) {
+//         let diff =  target - arr[i]
+
+//         if (map[diff] !== undefined) {
+//             return [map[diff], i];
+//         }
+
+//         map[arr[i]] = i;
+//     }
+    
+// }
+
+// let res = twoSum(arr, 9)
+// console.log(res)
+
+//! 2. Palindrome Check (String)
+
+// let str = "madam"
+
+// function isPalindrome(str) {
+//     let clean = str.toLowerCase();
+//     return clean === clean.split('').reverse().join('');
+// }
+
+// let res = isPalindrome(str)
+// console.log(res)
+
+//! 3. Find First Non-Repeating Character
+
+
