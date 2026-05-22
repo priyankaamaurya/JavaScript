96029066512e69184cf232937638391641a6701a
priyanka <prachii8826@gmail.com>
2026-05-22 15:04:24 +0530

    added files

--- Assessment/Assessment 7/script.js
+++ Assessment/Assessment 7/script.js
@@ -50,8 +50,10 @@
 
 //! 7. Promise.all
 
-let p1 = Promise.resolve(1);
-let p2 = Promise.resolve(2);
+// let p1 = Promise.resolve(1);
+// let p2 = Promise.resolve(2);
 
-Promise.all([p1, p2])
-  .then(res => console.log(res));
+// Promise.all([p1, p2])
+//   .then(res => console.log(res));
+
+//! 8.
